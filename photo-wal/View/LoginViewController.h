3f619d5c7736691e98f75a9a3e4dd921fcb1c3be
劉士豪 <liushihao@tuhaobook.local>
2017-05-12 17:43:37 +0800

    add login view

---
+++ photo-wal/View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  photo-wal
+//
+//  Created by 劉士豪 on 2017/5/12.
+//  Copyright © 2017年 劉士豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
